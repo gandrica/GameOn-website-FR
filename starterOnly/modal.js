fa82d8a6ad7fb2d3d51df0ec94d8a93ae784b46b
gandrica <gandrica1983@gmail.com>
2023-02-09 09:19:50 +0100

    This fixes #1 issue - click on the X bouton => close Modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,7 @@
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += "responsive";
   } else {
     x.className = "topnav";
   }
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//Close the modal button
+// 1 . Close button function
+const closeBtn = function () {
+  modalbg.style.display = "none";
+};
+// 2 . Event
+closeModalBtn.addEventListener("click", closeBtn);
