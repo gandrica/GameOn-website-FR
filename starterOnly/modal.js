89b6f0cbeedcbf0a0b208e13668da4925954e6e4
gandrica <gandrica1983@gmail.com>
2023-02-22 11:11:25 +0100

    This fixes #2 #3 and #4 issues

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
 const closeModalBtn = document.querySelector(".close");
 
 // launch modal event
@@ -21,10 +22,172 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//Close the modal button
-// 1 . Close button function
+//#1 - Fermer le modal avec le button X
+// Close button function
 const closeBtn = function () {
   modalbg.style.display = "none";
 };
-// 2 . Event
+//Close button event handler
 closeModalBtn.addEventListener("click", closeBtn);
+
+//#2 -Implémenter entrées du formulaire
+//Validate form data function
+
+const form = document.querySelector("form");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const tournoi = document.getElementById("tournoi");
+const checkbox1 = document.querySelector("#checkbox1");
+
+const checkName = function (name) {
+  const nameRegex = /^[a-zA-Z]{2,30}$/;
+  const isValid = nameRegex.test(name.value + "")
+    ? formValidation(name)
+    : formValidationError(name);
+  if (isValid) return true;
+  else return false;
+};
+
+const checkEmail = function (email) {
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  const isValid = emailRegex.test(email.value + "")
+    ? formValidation(email)
+    : formValidationError(email);
+  if (isValid) return true;
+  else return false;
+};
+
+const checkDate = function (dateInput) {
+  const isValid = !(dateInput.value === null || dateInput.value === "")
+    ? formValidation(birthdate)
+    : formValidationError(birthdate);
+  return isValid;
+};
+
+const checkNumber = function (number) {
+  const nameRegex = /^-?\d+$/;
+  const isValid =
+    number.value !== "" && nameRegex.test(+number.value)
+      ? formValidation(quantity)
+      : formValidationError(quantity);
+  return isValid;
+};
+
+const checkTournoi = function (element) {
+  const isValid = element.querySelector('input[type="radio"]:checked')
+    ? formValidation(element)
+    : formValidationError(element);
+  return isValid;
+};
+
+const checkCheckbox = function (element) {
+  const isValid = document.querySelector("#checkbox1:checked")
+    ? formValidation(element)
+    : formValidationError(element);
+  return isValid;
+};
+
+const createDivError = function (element, text) {
+  const divError = document.createElement("div");
+  divError.style.color = "red";
+  divError.style.display = "inline-block";
+  divError.style.margin = "0 0 6px 12px";
+  divError.style.fontSize = ".6em";
+  divError.classList.add(`error`);
+  const divErrorText = document.createTextNode(text);
+  divError.appendChild(divErrorText);
+  element.appendChild(divError);
+};
+
+const formValidation = function (element) {
+  if (element.id === "tournoi") {
+    const divErrorElement = tournoi.querySelector(`.error`);
+    if (divErrorElement) divErrorElement.style.display = "none";
+    return true;
+  } else {
+    const divErrorParent = element.parentNode.querySelector(`.error`);
+    if (divErrorParent) divErrorParent.style.display = "none";
+    return true;
+  }
+};
+
+const formValidationError = function (element) {
+  const elementID = element.id;
+  const parentElement = element.parentNode;
+
+  let divError = parentElement.querySelector(".error");
+  let divErrorElement = element.querySelector(".error");
+
+  if (divError) {
+    divError.style.display = "inline-block";
+  } else if (divErrorElement) {
+    divErrorElement.style.display = "inline-block";
+  } else {
+    if (elementID === "first") {
+      createDivError(
+        parentElement,
+        `Veuillez entrer 2 caractères ou plus pour le champ du prénom.`
+      );
+    } else if (elementID === "last") {
+      createDivError(
+        parentElement,
+        `Veuillez entrer 2 caractères ou plus pour le champ du nom.`
+      );
+    } else if (elementID === "email") {
+      createDivError(parentElement, "L'email n'est pas valide");
+    } else if (elementID === "birthdate") {
+      createDivError(
+        parentElement,
+        "Vous devez entrer votre date de naissance."
+      );
+    } else if (elementID === "quantity") {
+      createDivError(parentElement, "Saisissez une valeur numérique.");
+    } else if (elementID === "tournoi") {
+      createDivError(element, "Vous devez choisir une option.");
+    } else if (elementID === "checkbox1") {
+      createDivError(
+        parentElement,
+        "Vous devez vérifier que vous acceptez les termes et conditions."
+      );
+    }
+  }
+  return false;
+};
+
+const verifyInputs = function () {
+  checkTournoi(tournoi);
+  checkCheckbox(checkbox1);
+  checkNumber(quantity);
+  checkDate(birthdate);
+  checkEmail(email);
+  checkName(lastName);
+  checkName(firstName);
+
+  if (
+    checkTournoi(tournoi) &&
+    checkCheckbox(checkbox1) &&
+    checkNumber(quantity) &&
+    checkDate(birthdate) &&
+    checkEmail(email) &&
+    checkName(lastName) &&
+    checkName(firstName)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(verifyInputs());
+
+  if (verifyInputs()) {
+    alert("Merci ! Votre réservation a été reçue.");
+    modalbg.style.display = "none";
+  }
+});
